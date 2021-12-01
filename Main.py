40a565175006725d53f8140c7c92a9ae87f521ad
Katelynn Ma <58181197+katantikamayanotdimah@users.noreply.github.com>
2021-12-01 13:44:58 +0000

    Fight - gun - die

--- Main.py
+++ Main.py
@@ -1,1 +1,13 @@
+# replitPython
+print("Welome human.")
 
+print('It is the year 3000. Welcome to the zombie apocolypse')
+
+choice1 = input('Do you FIGHT or HIDE')
+
+if choice1 =="fight":
+  print("Okay then; prepare for battle.")
+
+  choice1a=input("Choose your weapon: GUN or BAT")
+  if choice1a=="gun":
+    print("You run out of bullets. All you have left are your bare    hands. You are not strong enough. The zombies beat you.    YOU DIE.")
