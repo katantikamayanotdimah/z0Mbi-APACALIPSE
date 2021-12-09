0bd023ac6d1d67ea945379704930d9a7f7a9b113
Katelynn Ma <58181197+katantikamayanotdimah@users.noreply.github.com>
2021-12-09 18:40:55 +0000

    most recent

--- Main.py
+++ Main.py
@@ -3,11 +3,18 @@ print("Welome human.")
 
 print('It is the year 3000. Welcome to the zombie apocolypse')
 
-choice1 = input('Do you FIGHT or HIDE')
+choice1 = input('Do you FIGHT or HIDE?')
 
-if choice1 =="fight":
+if choice1 =="FIGHT":
   print("Okay then; prepare for battle.")
 
-  choice1a=input("Choose your weapon: GUN or BAT")
+  choice1a=input("Choose your weapon: GUN or BAT?")
   if choice1a=="gun":
-    print("You run out o\f bullets. All you have left are your bare    hands. You are not strong enough. The zombies beat you.    YOU DIE.")
+    print("You run out of bullets. All you have left are your bare    hands. You are not strong enough. The zombies beat you.    YOU DIE.")
+
+elif choice1 =="HIDE":
+  print("Wise choice, *cough* coward *cough*")
+
+  choice2a=input("Will you hide in a STORE or HOUSE?")
+
+
