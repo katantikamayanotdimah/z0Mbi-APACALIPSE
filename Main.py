90ffd79f35147c65720140d8c726848f1c0fece5
Katelynn Ma <58181197+katantikamayanotdimah@users.noreply.github.com>
2021-12-09 18:58:27 +0000

    DONEEE

--- Main.py
+++ Main.py
@@ -9,12 +9,17 @@ if choice1 =="FIGHT":
   print("Okay then; prepare for battle.")
 
   choice1a=input("Choose your weapon: GUN or BAT?")
-  if choice1a=="gun":
+  if choice1a=="GUN":
     print("You run out of bullets. All you have left are your bare    hands. You are not strong enough. The zombies beat you.    YOU DIE.")
+  if choice1a=="BAT":
+    print("Bam. They are all dead. Not only have you survived, but you have killed all the zombies, saving humanity. Well done,  hero.")
 
 elif choice1 =="HIDE":
   print("Wise choice, *cough* coward *cough*")
 
   choice2a=input("Will you hide in a STORE or HOUSE?")
-
+  if choice2a =="STORE":
+    print("Very smart. You hide out with others who had the same idea as you. You are able to use the products in the store to   survive. Congrats. You lived. Well done.")
+  if choice2a =="HOUSE":
+    print("2 weeks pass. You run out of food. A zombie breaks in and  you are too weak to fight back. You die.")
 
